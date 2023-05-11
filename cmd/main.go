57ef155758f24ffe7df3c2a15190a0abacdb62c4
AlexStorm <info@alexstorm.ru>
2023-05-11 12:54:26 +0530

    was extracted  kube tools to library

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"flag"
 	"github.com/joho/godotenv"
+	"github.com/solenopsys/bl-kubernetes-tools"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/klog/v2"
 	"os"
-	"path/filepath"
 	"sc-bm-ipfs-revproxy/internal"
 	"sc-bm-ipfs-revproxy/pkg/utils"
 	"strings"
@@ -18,28 +16,6 @@ var Mode string
 
 const DEV_MODE = "dev"
 
-func getCubeConfig(devMode bool) (*rest.Config, error) {
-	if devMode {
-		var kubeconfigFile = os.Getenv("kubeconfigPath")
-		kubeConfigPath := filepath.Join(kubeconfigFile)
-		klog.Infof("Using kubeconfig: %s\n", kubeConfigPath)
-
-		kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
-		if err != nil {
-			klog.Error("error getting Kubernetes config: %v\n", err)
-			os.Exit(1)
-		}
-
-		return kubeConfig, nil
-	} else {
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			return nil, err
-		}
-
-		return config, nil
-	}
-}
 func init() {
 	flag.StringVar(&Mode, "mode", "", "a string var")
 }
@@ -57,7 +33,7 @@ func main() {
 
 	port := os.Getenv("server.Port")
 
-	config, err := getCubeConfig(devMode)
+	config, err := bl_kubernetes_tools.GetCubeConfig(devMode)
 	if err != nil {
 		klog.Info("Config init error...", err)
 		os.Exit(1)
